eca8be4c44fdf0d064eed0da002a7ba355fba5b6
Lucas Motta <lnoffs@hotmail.com>
2026-06-09 18:58:54 -0300

    Push from pc

--- lib/helper/statistic_builder_helper.dart
+++ lib/helper/statistic_builder_helper.dart
@@ -80,32 +80,33 @@ class StatisticBuilderHelper {
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text(
-                  "Progresso do album",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                Padding(
+                  padding: EdgeInsetsGeometry.only(left: 20.0, top: 20.0),
+                  child: Text(
+                    "Progresso do album",
+                    style: GoogleFonts.inter(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ],
             ),
             CircularPercentIndicator(
-              radius: 120.0,
+              radius: 80.0,
               animation: true,
               animationDuration: 1200,
               lineWidth: 15.0,
               percent: data.progressionPercentage,
               center: Text(
                 "${(data.progressionPercentage * 100).round()}%",
-                style: GoogleFonts.inter(fontSize: 60)
-                //TextStyle(fontWeight: FontWeight.bold, fontSize: 40.0, color: Colors.white, fontFamily: "Inter"),
+                style: GoogleFonts.inter(fontSize: 50, color: Colors.white),
               ),
               circularStrokeCap: CircularStrokeCap.butt,
               backgroundColor: Color.fromRGBO(45, 63, 84, 1),
               progressColor: const Color.fromRGBO(0, 212, 198, 1),
             ),
             Card(
-              
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(
                   20.0,
@@ -115,19 +116,30 @@ class StatisticBuilderHelper {
                 text: TextSpan(
                   style: GoogleFonts.inter(),
                   children: <TextSpan>[
-                    TextSpan(text: "Faltam", style: GoogleFonts.inter(fontSize: 25)),
                     TextSpan(
-                      text: " ${data.missingCards} ",
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 45, 233, 240),
+                      text: "Faltam",
+                      style: GoogleFonts.inter(
+                        fontSize: 17,
+                        color: Color.fromRGBO(119, 153, 192, 1),
                       ),
                     ),
-                    TextSpan(text: " figurinhas para completar!", style: GoogleFonts.inter(fontSize: 25)),
+                    TextSpan(
+                      text: " ${data.missingCards} ",
+                      style: GoogleFonts.inter(
+                        color: const Color.fromARGB(255, 45, 233, 240),
+                        fontSize: 23,
+                      ),
+                    ),
+                    TextSpan(
+                      text: " figurinhas para completar!",
+                      style: GoogleFonts.inter(
+                        fontSize: 17,
+                        color: Color.fromRGBO(119, 153, 192, 1),
+                      ),
+                    ),
                   ],
                 ),
               ),
-
-              //child: Text("Faltam ${data.missingCards} figurinhas para completar!"),
             ),
           ],
         ),
